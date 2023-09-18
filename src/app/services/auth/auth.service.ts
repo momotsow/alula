726432499597c8af2ea259ee63fb31c35d955639
momotsow <kgomotsomkansi@gmail.com>
2023-09-18 12:07:23 +0200

    Code files

--- src/app/services/auth/auth.service.ts
+++ src/app/services/auth/auth.service.ts
@@ -22,20 +22,34 @@ export class AuthService {
 
   constructor(private http: HttpClient) {}
 
-  // Register a user
+  private saveUsers(data: User[]): Observable<any> {
+    return this.http.put(this.usersUrl, data).pipe(
+      catchError((error) => {
+        console.error('Error saving user data:', error);
+        return throwError('Error saving user data');
+      })
+    );
+  }
+
   register(user: User): Observable<any> {
     debugger;
     return this.getUsers().pipe(
-      map((data: User[]) => {
+      mergeMap((data: User[]) => {
         const existingUser = data.find((u) => u.email === user.email);
         if (existingUser) {
-          return { success: false, message: 'User already exists' };
+          console.log("user already exists")
         }
 
         data.push(user);
-        this.saveUsers([user]);
 
-        return { success: true, message: 'Registration successful' };
+        // Save the updated data back to the JSON file
+        return this.saveUsers(data).pipe(
+          map(() => ({ success: true, message: 'Registration successful' })),
+          catchError((error) => {
+            console.error('Error saving user data:', error);
+            return throwError('Error saving user data');
+          })
+        );
       })
     );
   }
@@ -70,30 +84,5 @@ export class AuthService {
   private getUsers(): Observable<User[]> {
     return this.http.get<User[]>(this.usersUrl);
   }
-
-  private saveUsers(newUser: User): Observable<any> {
-    return this.http.get<any[]>(this.usersUrl).pipe(
-      catchError((error) => {
-        console.error('Error retrieving user data:', error);
-        return throwError('Error retrieving user data');
-      }),
-      mergeMap((data: any[]) => {
-        // Ensure that the data is an array and cast it to User[] if needed
-        const userData: User[] = Array.isArray(data) ? data : [];
-
-        // Add the new user object to the array
-        userData.push(newUser);
-
-        // Save the updated data back to the JSON file
-        return this.http.put(this.usersUrl, userData).pipe(
-          catchError((error) => {
-            console.error('Error saving user data:', error);
-            return throwError('Error saving user data');
-          })
-        );
-      })
-    );
-  }
-
 
 }
